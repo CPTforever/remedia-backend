3fb93deabfb4d35a005222852ba943b8cac15316
Albert Lee <allilee@ucsc.edu>
2022-01-14 21:39:55 -0800

    Second iteration, added all drug info

--- webscraper.py
+++ webscraper.py
@@ -9,9 +9,9 @@ Major league hacking
 import requests
 from bs4 import BeautifulSoup
 
-drug = "Acarbose"
+drug = "advil"
 URL = "https://vsearch.nlm.nih.gov/vivisimo/cgi-bin/query-meta?v%3Aproject=medlineplus&v%3Asources=medlineplus-bundle&query={}&binning-state=group%3d%3dDrugs%20and%20Supplements&".format(drug)
-#print(URL)
+#URL)
 nlmsearch =  "https://vsearch.nlm.nih.gov"
 drugplus = URL + drug
 
@@ -21,9 +21,22 @@ soup = BeautifulSoup(page.content, "html.parser")
 document = soup.find(id="document-list")
 results = document.find("ol", class_="results")
 drugURL = nlmsearch + results.div.a.get('href')
-print(drugURL)
+
 
 drugPage = requests.get(drugURL)
 soup = BeautifulSoup(drugPage.content, "html.parser")
-print(soup)
-#print(job_elements)
+
+why = soup.article.find("div", id="why")
+how = soup.article.find("div", id="how")
+other = soup.article.find("div", id="other-uses")
+care = soup.article.find("div", id="precautions")
+diet = soup.article.find("div", id="special-dietary")
+forget = soup.article.find("div", id="if-i-forget")
+store = soup.article.find("div", id="storage-conditions")
+side = soup.article.find("div", id="side-effects")
+over = soup.article.find("div", id="overdose")
+other = soup.article.find("div", id="other-information")
+
+
+    #x.prettify())
+#job_elements)
